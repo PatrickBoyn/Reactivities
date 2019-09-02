8c5055e2c6b05541b9fcafc172ad4bc8534d37a9
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-09-02 12:51:39 -0400

    Added the mediatr classes, and a handle method.

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,7 +1,29 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Persistence;
+
 namespace Application.Activities
 {
     public class List
     {
-        
+        public class Query : IRequest<List<Activity>> { }
+
+        public class Handler : IRequestHandler<Query, List<Activity>>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
     }
 }
