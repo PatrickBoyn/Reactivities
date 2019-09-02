650ff6b89ce31bf9a3d8092f6b3338ae75feb5ae
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-09-02 14:55:21 -0400

    Added a Query class with a prop.

--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -1,7 +1,14 @@
+using System;
+using Domain;
+using MediatR;
+
 namespace Application.Activities
 {
     public class Details
     {
-        
+        public class Query : IRequest<Activity>
+        {
+            public Guid Id { get; set; }
+        }
     }
 }
