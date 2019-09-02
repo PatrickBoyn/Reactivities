b83bc45b3f2ccfafce83fc56ba3116917d2b73ff
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-09-02 18:19:24 -0400

    Added the props, and a nested class.

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using MediatR;
 
 namespace Application.Activities
@@ -6,7 +9,21 @@ namespace Application.Activities
     {
         public class Command : IRequest
         {
+            public Guid Id { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public string Category { get; set; }
+            public DateTime Date { get; set; }
+            public string City { get; set; }
+            public string Venue { get; set; }
+        }
 
+        public class Handler : IRequestHandler<Command>
+        {
+            public Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
