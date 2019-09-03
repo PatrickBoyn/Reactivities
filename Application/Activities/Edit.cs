835b7318dc1b87a575bac103aa30e153970c5e65
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-09-03 01:44:14 -0400

    Added logic for editing the activity.

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -14,7 +14,7 @@ namespace Application.Activities
             public string Title { get; set; }
             public string Description { get; set; }
             public string Category { get; set; }
-            public DateTime Date { get; set; }
+            public DateTime? Date { get; set; }
             public string City { get; set; }
             public string Venue { get; set; }
         }
@@ -29,8 +29,19 @@ namespace Application.Activities
 
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
+                Domain.Activity activity = await _context.Activities.FindAsync(request.Id);
 
-                var success = await _context.SaveChangesAsync() > 0;
+                if (activity == null)
+                    throw new Exception("Could not find activity.");
+
+                activity.Title = request.Title ?? activity.Title;
+                activity.Description = request.Description ?? activity.Description;
+                activity.Category = request.Category ?? activity.Category;
+                activity.Date = request.Date ?? activity.Date;
+                activity.City = request.City ?? activity.City;
+                activity.Venue = request.Venue ?? activity.Venue;
+
+                bool success = await _context.SaveChangesAsync() > 0;
 
                 if (success)return Unit.Value;
 
