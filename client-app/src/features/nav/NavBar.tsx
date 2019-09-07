b7a57465bea748b7553f3898ada3ad71db181d61
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-09-06 22:43:13 -0400

    Added the navbar HTML.

---
+++ client-app/src/features/nav/NavBar.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Menu, Container, Button } from 'semantic-ui-react';
+
+const NavBar = () => {
+  return (
+    <div>
+      <Menu fixed='top' inverted>
+        <Container>
+          <Menu.Item header>
+            <img src='/assets/logo.png' alt='Logo' />
+            Reactivities
+          </Menu.Item>
+          <Menu.Item name='Activities' />
+          <Menu.Item>
+            <Button positive content='CreateActivity' />
+          </Menu.Item>
+        </Container>
+      </Menu>
+    </div>
+  );
+};
+
+export default NavBar;
