b7a46cb83ef4710316959e69fb26c9cf7c0fb489
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-09-08 02:31:51 -0400

    Passed the activities down to the AL file.

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -11,7 +11,7 @@ const ActivityDashboard: React.FC<IProps> = ({ activities }) => {
   return (
     <Grid>
       <Grid.Column width={10}>
-        <ActivityList />
+        <ActivityList activities={activities} />
         {/* <List>
           {activities.map(activity => (
             <List.Item key={activity.id}>{activity.title}</List.Item>
