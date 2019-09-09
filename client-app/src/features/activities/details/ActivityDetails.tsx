030212b6f4bb0ec9dd3c68af8de9ffd52b661eb2
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-09-09 01:54:56 -0400

    Filled out the Activity details section.

---
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Card, Image, Icon, Button } from 'semantic-ui-react';
+
+const ActivityDetails = () => {
+  return (
+    <Card fluid>
+      <Image
+        src='/client-app/public/assets/placeholder.png'
+        wrapped
+        ui={false}
+      />
+      <Card.Content>
+        <Card.Header>Title</Card.Header>
+        <Card.Meta>
+          <span>Date</span>
+        </Card.Meta>
+        <Card.Description>Description</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Button.Group widths={2}>
+          <Button basic color='blue' content='edit' />
+          <Button basic color='grey' content='cancel' />
+        </Button.Group>
+      </Card.Content>
+    </Card>
+  );
+};
+
+export default ActivityDetails;
