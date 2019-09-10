36d7f033ed548c3dded2a131055e260d182755c9
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-09-10 02:28:46 -0400

    Added the forms inputs.

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Segment, Form } from 'semantic-ui-react';
 
 const ActivityForm = () => {
-  return <div></div>;
+  return (
+    <Segment>
+      <Form>
+        <Form.Input placeholder='title' />
+        <Form.Input placeholder='description' />
+        <Form.Input placeholder='Category' />
+        <Form.Input placeholder='Date' />
+        <Form.Input placeholder='City' />
+        <Form.Input placeholder='Venue' />
+      </Form>
+    </Segment>
+  );
 };
 
 export default ActivityForm;
