7b7ce18b822fbabe133e032a5d192d5aa03d9978
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-09-11 01:41:16 -0400

    Set the activity details to only be displayed if it's not null.

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -7,16 +7,23 @@ import ActivityForm from '../form/ActivityForm';
 
 interface IProps {
   activities: IActivity[];
+  selectActivity: (id: string) => void;
+  selectedActivity: IActivity | null;
 }
 
-const ActivityDashboard: React.FC<IProps> = ({ activities }) => {
+const ActivityDashboard: React.FC<IProps> = ({
+  activities,
+  selectActivity,
+  selectedActivity,
+}) => {
   return (
     <Grid>
       <Grid.Column width={10}>
-        <ActivityList activities={activities} />
+        <ActivityList activities={activities} selectActivity={selectActivity} />
       </Grid.Column>
       <Grid.Column width={6}>
-        <ActivityDetails />
+        {/* Displays the activity if it's not null. */}
+        {selectedActivity && <ActivityDetails activity={selectedActivity} />}
         <ActivityForm />
       </Grid.Column>
     </Grid>
