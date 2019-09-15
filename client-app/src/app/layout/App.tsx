3d572306b79666a68e055e97186b803b390bfe2b
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-09-15 02:55:28 -0400

    Added methods to handle creating and editing activities.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -20,6 +20,18 @@ const App = () => {
     setSelectedActivity(null);
     setEditMode(true);
   }
+  
+  const handleCreateActivity = (activity: IActivity) => {
+    setActivities([...activities, activity]);
+    setSelectedActivity(activity);
+    setEditMode(false);
+  }
+  
+  const handleEditActivity = (activity: IActivity) => {
+    setActivities([...activities.filter(a => a.id !== activity.id), activity]);
+    setSelectedActivity(activity);
+    setEditMode(false);
+  }
 
   useEffect(() => {
     axios.get('http://localhost:5000/api/activities').then(response => {
@@ -38,6 +50,8 @@ const App = () => {
           editMode={editMode}
           setEditMode={setEditMode}
           setSelectedActivity={setSelectedActivity}
+          createActivity={handleCreateActivity}
+          editActivity={handleEditActivity}
         />
       </Container>
     </Fragment>
