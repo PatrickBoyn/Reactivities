6145f955f6c147871bcf0d48e47d60dbfe8556a5
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-09-15 02:56:06 -0400

    Added the create and edit activities to the form.

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -12,6 +12,8 @@ interface IProps {
   editMode: boolean;
   setEditMode: (editMode: boolean) => void;
   setSelectedActivity: (activity: IActivity | null) => void;
+  createActivity: (activity: IActivity) => void;
+  editActivity: (activity: IActivity) => void;
 }
 
 const ActivityDashboard: React.FC<IProps> = ({
@@ -20,7 +22,9 @@ const ActivityDashboard: React.FC<IProps> = ({
   selectedActivity,
   editMode,
   setEditMode,
-  setSelectedActivity  
+  setSelectedActivity,
+  createActivity,
+  editActivity,  
 }) => {
   return (
     <Grid>
@@ -38,7 +42,9 @@ const ActivityDashboard: React.FC<IProps> = ({
         )}
         {editMode && <ActivityForm 
             setEditMode={setEditMode}
-            activity={selectedActivity!}/>}
+            activity={selectedActivity!} 
+            createActivity={createActivity}
+            editActivity={editActivity}/>}
       </Grid.Column>
     </Grid>
   );
