35bb466f370f382cd5d3307876f93adb73e08d1e
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-09-15 22:51:24 -0400

    Split the seconds off the date time. Set edito mode to false in sa.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,61 +1,68 @@
-import React, { useState, useEffect, Fragment } from 'react';
-import { Container } from 'semantic-ui-react';
+import React, {useState, useEffect, Fragment} from 'react';
+import {Container} from 'semantic-ui-react';
 import axios from 'axios';
-import { IActivity } from '../models/activity';
+import {IActivity} from '../models/activity';
 import NavBar from '../../features/nav/NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 
 const App = () => {
-  const [activities, setActivities] = useState<IActivity[]>([]);
-  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(
-    null
-  );
-  const [editMode, setEditMode] = useState(false);
+    const [activities, setActivities] = useState<IActivity[]>([]);
+    const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(
+        null
+    );
+    const [editMode, setEditMode] = useState(false);
 
-  const handleSelectActivity = (id: string) => {
-    setSelectedActivity(activities.filter(a => a.id === id)[0]);
-  };
-  
-  const handleOpenCreateForm = () => {
-    setSelectedActivity(null);
-    setEditMode(true);
-  }
-  
-  const handleCreateActivity = (activity: IActivity) => {
-    setActivities([...activities, activity]);
-    setSelectedActivity(activity);
-    setEditMode(false);
-  }
-  
-  const handleEditActivity = (activity: IActivity) => {
-    setActivities([...activities.filter(a => a.id !== activity.id), activity]);
-    setSelectedActivity(activity);
-    setEditMode(false);
-  }
+    const handleSelectActivity = (id: string) => {
+        setSelectedActivity(activities.filter(a => a.id === id)[0]);
+        setEditMode(false);
+    };
 
-  useEffect(() => {
-    axios.get('http://localhost:5000/api/activities').then(response => {
-      setActivities(response.data);
-    });
-  }, []);
+    const handleOpenCreateForm = () => {
+        setSelectedActivity(null);
+        setEditMode(true);
+    };
 
-  return (
-    <Fragment>
-      <NavBar openCreateForm={handleOpenCreateForm}/>
-      <Container style={{ marginTop: '7em' }}>
-        <ActivityDashboard
-          activities={activities}
-          selectActivity={handleSelectActivity}
-          selectedActivity={selectedActivity}
-          editMode={editMode}
-          setEditMode={setEditMode}
-          setSelectedActivity={setSelectedActivity}
-          createActivity={handleCreateActivity}
-          editActivity={handleEditActivity}
-        />
-      </Container>
-    </Fragment>
-  );
+    const handleCreateActivity = (activity: IActivity) => {
+        setActivities([...activities, activity]);
+        setSelectedActivity(activity);
+        setEditMode(false);
+    };
+
+    const handleEditActivity = (activity: IActivity) => {
+        setActivities([...activities.filter(a => a.id !== activity.id), activity]);
+        setSelectedActivity(activity);
+        setEditMode(false);
+    };
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/activities').then(response => {
+            let activities: IActivity[] = [];
+            
+            response.data.forEach((activity: IActivity) => {
+               activity.date = activity.date.split('.')[0]; 
+            });
+            
+            setActivities(response.data);
+        });
+    }, []);
+
+    return (
+        <Fragment>
+            <NavBar openCreateForm={handleOpenCreateForm}/>
+            <Container style={{marginTop: '7em'}}>
+                <ActivityDashboard
+                    activities={activities}
+                    selectActivity={handleSelectActivity}
+                    selectedActivity={selectedActivity}
+                    editMode={editMode}
+                    setEditMode={setEditMode}
+                    setSelectedActivity={setSelectedActivity}
+                    createActivity={handleCreateActivity}
+                    editActivity={handleEditActivity}
+                />
+            </Container>
+        </Fragment>
+    );
 };
-
+//Ignore this warning if you want the app to work.
 export default App;
