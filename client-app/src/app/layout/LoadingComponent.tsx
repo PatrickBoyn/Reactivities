1af150ac3ac8817211ca6a5e827cf20020f935db
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-09-17 17:45:47 -0400

    Minor formatting and set inverted to true.

--- client-app/src/app/layout/LoadingComponent.tsx
+++ client-app/src/app/layout/LoadingComponent.tsx
@@ -1,9 +1,9 @@
 ﻿import React from 'react';
 import {Dimmer, Loader} from "semantic-ui-react";
 
-const LoadingComponent: React.FC<{inverted?: boolean, content?: string}> = ({
-    inverted, content
-                                                                            }) => {
+const LoadingComponent: React.FC<{
+    inverted?: boolean, 
+    content?: string}> = ({inverted=true, content}) => {                                                      
     return (
         <Dimmer active inverted={inverted}> 
             <Loader content={content}/>
