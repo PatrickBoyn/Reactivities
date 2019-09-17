55b3df8775a0a4496afef6b4a88e039e2d728d08
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-09-17 17:47:23 -0400

    Set the loader to work while loading is true.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -4,6 +4,7 @@ import {IActivity} from '../models/activity';
 import NavBar from '../../features/nav/NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import agent from "../api/agent";
+import LoadingComponent from "./LoadingComponent";
 
 const App = () => {
     const [activities, setActivities] = useState<IActivity[]>([]);
@@ -11,7 +12,8 @@ const App = () => {
         null
     );
     const [editMode, setEditMode] = useState(false);
-
+    const [loading, setLoading] = useState(true);
+    
     const handleSelectActivity = (id: string) => {
         setSelectedActivity(activities.filter(a => a.id === id)[0]);
         setEditMode(false);
@@ -53,9 +55,11 @@ const App = () => {
                     activities.push(activity);
                 });
                 setActivities(activities);
-            })
+            }).then(() => setLoading(false));
     }, []);
 
+    if (loading) return <LoadingComponent content='Loading activities...'/>;
+    
     return (
         <Fragment>
             <NavBar openCreateForm={handleOpenCreateForm}/>
