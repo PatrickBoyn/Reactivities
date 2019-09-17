6ea41153c4f5486dcfd7e792542117b63c143d46
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-09-17 18:13:54 -0400

    Added the boolean to the button.

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -8,6 +8,7 @@ interface IProps {
   activity: IActivity;
   createActivity: (activity: IActivity) => void;
   editActivity: (activity: IActivity) => void;
+  submitting: boolean;
 }
 
 
@@ -16,7 +17,9 @@ const ActivityForm: React.FC<IProps> = (
       setEditMode, 
       activity: initialFormState,
       createActivity,
-      editActivity}) => {
+      editActivity,
+      submitting
+    }) => {
   const initializeForm = () => {
     if (initialFormState){
       return initialFormState;
@@ -87,7 +90,12 @@ const ActivityForm: React.FC<IProps> = (
             name='venue'
             placeholder='Venue' 
             value={activity.venue}/>
-        <Button floated='right' positive type='submit' content='Submit'/>
+        <Button
+            loading={submitting}    
+            floated='right' 
+            positive 
+            type='submit' 
+            content='Submit'/>
         <Button onClick={() => setEditMode(false)} floated='right' type='button' content='Cancel'/>
       </Form>
     </Segment>
