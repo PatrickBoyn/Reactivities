7dd8f85a7ad7efa080dbe2ff2eb08619416bebc9
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-09-18 16:12:06 -0400

    Used the activity store to handle activities, and select activity.

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,16 +1,15 @@
-import React, {SyntheticEvent} from 'react';
+import React, {SyntheticEvent, useContext} from 'react';
 import { Grid } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
 import ActivityList from './ActivityList';
 import ActivityDetails from '../details/ActivityDetails';
 import ActivityForm from '../form/ActivityForm';
 import {observer} from "mobx-react-lite";
+import ActivityStore from '../../../app/stores/activityStore';
 
 interface IProps {
   activities: IActivity[];
   selectActivity: (id: string) => void;
-  selectedActivity: IActivity | null;
-  editMode: boolean;
   setEditMode: (editMode: boolean) => void;
   setSelectedActivity: (activity: IActivity | null) => void;
   createActivity: (activity: IActivity) => void;
@@ -21,10 +20,6 @@ interface IProps {
 }
 
 const ActivityDashboard: React.FC<IProps> = ({
-  activities,
-  selectActivity,
-  selectedActivity,
-  editMode,
   setEditMode,
   setSelectedActivity,
   createActivity,
@@ -33,12 +28,12 @@ const ActivityDashboard: React.FC<IProps> = ({
   submitting,
   target  
 }) => {
+  const activityStore = useContext(ActivityStore);
+  const { editMode, selectedActivity} = activityStore;
   return (
     <Grid>
       <Grid.Column width={10}>
-        <ActivityList 
-            activities={activities} 
-            selectActivity={selectActivity} 
+        <ActivityList
             deleteActivity={deleteActivity}
             submitting={submitting}
             target={target}/>
@@ -47,7 +42,6 @@ const ActivityDashboard: React.FC<IProps> = ({
         {/* Displays the activity if it's not null. */}
         {selectedActivity && !editMode && (
           <ActivityDetails
-            activity={selectedActivity}
             setEditMode={setEditMode}
             setSelectedActivity={setSelectedActivity}
           />
