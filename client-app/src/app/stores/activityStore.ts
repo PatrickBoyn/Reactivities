bc1470eac9da98ee7abc3efe6a871ed806596dbe
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-09-18 02:42:04 -0400

    Added a test observable.

---
+++ client-app/src/app/stores/activityStore.ts
@@ -0,0 +1,8 @@
+﻿import {observable} from "mobx";
+import {createContext} from "react";
+
+class ActivityStore {
+    @observable title = 'Hello from mobx';
+}
+
+export default createContext(new ActivityStore())
