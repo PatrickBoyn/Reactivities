6bc1ae547eafcacfd1d81f7f0eedf5e6bb82d2c4
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-09-18 18:58:19 -0400

    Added methods for loading, creating, activities and opening a form.

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -8,23 +8,46 @@ class ActivityStore {
     @observable selectedActivity: IActivity | undefined;
     @observable loadingInitial = false;
     @observable editMode = false;
+    @observable submitting = false;
     
-    @action loadActivities = () => {
+    @action loadActivities = async () => {
         this.loadingInitial = true;
-        
-        agent.Activities.list()
-            .then(activities => {
-                activities.forEach(activity => {
-                    activity.date = activity.date.split('.')[0];
-                    this.activities.push(activity)
-                })
-            }).finally(() => this.loadingInitial = false);
-    }
+        try {
+            const activities = await agent.Activities.list();
+            activities.forEach(activity => {
+                activity.date = activity.date.split('.')[0];
+                this.activities.push(activity);
+            });
+            this.loadingInitial = false;
+        } catch (error) {
+            console.error(error);
+            this.loadingInitial = false;
+        }
+    };
+    
+    @action createActivity = async (activity: IActivity) => {
+        this.submitting = true;
+        try {
+            await agent.Activities.create(activity);
+            
+            this.activities.push(activity);
+            this.editMode = false;
+            this.submitting = false;
+        } catch (error) {
+            this.submitting = false;
+            console.error(error);
+        }
+    };
+    
+    @action openCreateForm = () => {
+        this.editMode = true;
+        this.selectedActivity = undefined;
+    };
     
     @action selectActivity = (id: string) => {
         this.selectedActivity = this.activities.find(a => a.id === id);
         this.editMode = false;
-    }
+    };
 }
 
 export default createContext(new ActivityStore())
