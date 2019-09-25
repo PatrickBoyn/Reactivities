085328138a8e8905219dd5db1004d76af736ee63
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-09-25 17:56:44 -0400

    Added a method to get a single activity, and changed the name of selectedActivity.

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -8,7 +8,7 @@ configure({enforceActions: 'always'});
 class ActivityStore {
     @observable activityRegistry = new Map();
     @observable activities: IActivity[] = [];
-    @observable selectedActivity: IActivity | undefined;
+    @observable activity: IActivity | undefined;
     @observable loadingInitial = false;
     @observable editMode = false;
     @observable submitting = false;
@@ -37,6 +37,32 @@ class ActivityStore {
         }
     };
     
+    @action loadActivity = async (id: string) => {
+        let activity = this.getActivity(id);
+        if (activity) 
+        {
+            this.activity = activity
+        } else {
+            this.loadingInitial = true;
+            try {
+                activity = await agent.Activities.details(id);
+                runInAction('getting activity', () => {
+                    this.activity = activity;
+                    this.loadingInitial = false;
+                });
+            } catch (error) {
+                runInAction('get activity error', () => {
+                    this.loadingInitial = false;
+                });
+                console.error(error);
+            }
+        }
+    };
+    
+    getActivity = (id: string) => {
+        return this.activityRegistry.get(id);
+    };
+    
     @action createActivity = async (activity: IActivity) => {
         this.submitting = true;
         try {
@@ -60,7 +86,7 @@ class ActivityStore {
             await agent.Activities.update(activity);
             runInAction('Editing Activity', () => {
                 this.activityRegistry.set(activity.id, activity);
-                this.selectedActivity = activity;
+                this.activity = activity;
                 this.editMode = false;
                 this.submitting = false; 
             });
@@ -92,16 +118,16 @@ class ActivityStore {
     
     @action openCreateForm = () => {
         this.editMode = true;
-        this.selectedActivity = undefined;
+        this.activity = undefined;
     };
     
     @action openEditForm = (id: string) => {
-        this.selectedActivity = this.activityRegistry.get(id);
+        this.activity = this.activityRegistry.get(id);
         this.editMode = true;
     };
     
     @action cancelSelectedActivity = () => {
-        this.selectedActivity = undefined;
+        this.activity = undefined;
     };
     
     @action cancelFormOpen = () => {
@@ -109,7 +135,7 @@ class ActivityStore {
     };
     
     @action selectActivity = (id: string) => {
-        this.selectedActivity = this.activityRegistry.get(id);
+        this.activity = this.activityRegistry.get(id);
         this.editMode = false;
     };
 }
