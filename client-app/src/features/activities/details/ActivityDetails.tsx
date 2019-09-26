5e6b3ca816bc72d0039955e0fafc5b863e25ef86
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-09-26 02:43:55 -0400

    Added the proper routing to the button.

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -2,7 +2,7 @@ import React, {useContext, useEffect} from 'react';
 import { Card, Image, Button } from 'semantic-ui-react';
 import ActivityStore from '../../../app/stores/activityStore';
 import {observer} from "mobx-react-lite";
-import { RouteComponentProps } from 'react-router-dom';
+import { RouteComponentProps, Link } from 'react-router-dom';
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 
 interface IDetailParams {
@@ -36,7 +36,8 @@ const ActivityDetails: React.FC<RouteComponentProps<IDetailParams>> = ({match, h
       <Card.Content extra>
         <Button.Group widths={2}>
           <Button
-            onClick={() => openEditForm(activity!.id)}
+            as={Link}
+            to={`/manage/${activity.id}`}
             basic
             color='blue'
             content='edit'
