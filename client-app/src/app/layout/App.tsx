d59fa470a982ee77d7a5b043f2ffccd374c7c50e
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-09-26 18:37:17 -0400

    Added a key so that the route knew what to do.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,16 +1,16 @@
-import React, { useEffect, Fragment, useContext} from 'react';
+import React, { useEffect, Fragment, useContext } from 'react';
 import {Container} from 'semantic-ui-react';
 import NavBar from '../../features/nav/NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import LoadingComponent from "./LoadingComponent";
 import ActivityStore from '../stores/activityStore';
 import {observer} from "mobx-react-lite";
-import { Route } from 'react-router-dom';
+import { Route, withRouter, RouteComponentProps } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage'
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
 
-const App = () => {
+const App: React.FC<RouteComponentProps> = ({location}) => {
     const activityStore = useContext(ActivityStore);
 
     useEffect(() => {
@@ -26,10 +26,12 @@ const App = () => {
                 <Route exact path='/' component={HomePage}/>
                 <Route exact path='/activities' component={ActivityDashboard}/>
                 <Route path='/activities/:id' component={ActivityDetails}/>
-                <Route path={['/createActivity', '/manage/:id']} component={ActivityForm}/>
+                <Route  key={location.key}
+                       path={['/createActivity', '/manage/:id']} 
+                       component={ActivityForm}/>
             </Container>
         </Fragment>
     );
 };
 //Ignore this warning if you want the app to work.
-export default observer(App);
+export default withRouter(observer(App));
