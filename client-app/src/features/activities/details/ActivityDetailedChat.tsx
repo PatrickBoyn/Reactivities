957dc862c6e91c86b9c2e0815bdf9714ffed249c
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-10-02 16:37:27 -0400

    Added a basic functional component.

---
+++ client-app/src/features/activities/details/ActivityDetailedChat.tsx
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+
+const ActivityDetailedChat = () => {
+    return (
+        <div>
+            <h1>Chat</h1>
+        </div>
+    )
+};
+
+export default ActivityDetailedChat
---
+++ client-app/src/features/activities/details/ActivityDetailedHeader.tsx
@@ -0,0 +1,9 @@
+﻿import * as React from 'react';
+
+export const ActivityDetailedHeader = () => {
+    return (
+        <div>
+            <h1>Header</h1>
+        </div>
+    );
+};
