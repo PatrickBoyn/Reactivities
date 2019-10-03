60e0f05b383e7416843c2dec6a27be08bff204a2
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-10-03 14:47:34 -0400

    Added the layout and styling for the info section.

--- client-app/src/features/activities/details/ActivityDetailedInfo.tsx
+++ client-app/src/features/activities/details/ActivityDetailedInfo.tsx
@@ -1,11 +1,48 @@
 ﻿import React from 'react';
+import { Grid, Icon, Segment } from 'semantic-ui-react';
+import { IActivity } from "../../../app/models/activity";
 
-const ActivityDetailedInfo = () => {
+const divPadding = {
+    padding: 0,
+};
+
+const ActivityDetailedInfo: React.FC<{ activity: IActivity }> = ({ activity }) => {
     return (
-        <div>
-            <h1>Info</h1>
-        </div>
+        <Segment.Group>
+            <Segment attached='top'>
+                <Grid>
+                    <Grid.Column width={1}>
+                        <Icon size='large' color='teal' name='info'/>
+                    </Grid.Column>
+                    <Grid.Column width={15}>
+                        <p>{ activity.description }</p>
+                    </Grid.Column>
+                </Grid>
+            </Segment>
+            <Segment attached>
+                <Grid verticalAlign='middle'>
+                    <Grid.Column width={1}>
+                        <Icon name='calendar' size='large' color='teal'/>
+                    </Grid.Column>
+                    <Grid.Column width={15}>
+                        <span>
+                            { activity.date }
+                        </span>
+                    </Grid.Column>
+                </Grid>
+                <Segment attached>
+                    <Grid verticalAlign='middle'>
+                        <Grid.Column width={1} style={divPadding}>
+                            <Icon name='marker' size='large' color='teal'/>
+                        </Grid.Column>
+                        <Grid.Column width={11} style={divPadding}>
+                            <span>{ activity.venue }, { activity.city }</span>
+                        </Grid.Column>
+                    </Grid>
+                </Segment>
+            </Segment>
+        </Segment.Group>
     )
 };
 
-export default ActivityDetailedInfo
+export default ActivityDetailedInfo;
