5c17e66c5d7eeb56fa6c34a55a4b6ea0cace06cc
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-10-03 17:43:34 -0400

    Added the layout and styling for the chat.

--- client-app/src/features/activities/details/ActivityDetailedChat.tsx
+++ client-app/src/features/activities/details/ActivityDetailedChat.tsx
@@ -1,10 +1,55 @@
-﻿import React from 'react';
+﻿import React, { Fragment } from 'react';
+import {
+    Header,
+    Segment,
+    CommentGroup,
+    CommentAvatar,
+    Comment,
+    CommentAuthor,
+    CommentContent, CommentMetadata, CommentText, CommentActions, Form, FormTextArea, Button
+} from "semantic-ui-react";
 
 const ActivityDetailedChat = () => {
     return (
-        <div>
-            <h1>Chat</h1>
-        </div>
+        <Fragment>
+            <Segment textAlign='center'
+                     attached='top'
+                     inverted
+                     color='teal'
+                     style={{border: 'none'}}>
+                <Header>Chat about this event</Header>
+            </Segment>
+            <CommentGroup>
+                <Comment>
+                    <CommentAvatar src='/assets/user.png'/>
+                    <CommentContent>
+                        <CommentAuthor as='a'>Matt</CommentAuthor>
+                        <CommentMetadata>
+                            <div>Today at 5:42 pm</div>
+                        </CommentMetadata>
+                        <CommentText>How artistic!</CommentText>
+                        <CommentActions>Reply</CommentActions>
+                    </CommentContent>
+                </Comment>
+    
+                <Comment>
+                    <CommentAvatar src='/assets/user.png'/>
+                    <CommentContent>
+                        <CommentAuthor as='a'>Joe Henderson</CommentAuthor>
+                        <CommentMetadata>
+                            <div>5 days ago</div>
+                        </CommentMetadata>
+                        <CommentText>Dude, this is aweosme! Thanks so much.</CommentText>
+                        <CommentActions>Reply</CommentActions>
+                    </CommentContent>
+                </Comment>
+    
+                <Form reply>
+                    <Form.TextArea />
+                    <Button content='Add Reply' labelPosition='left' icon='edit' primary />
+                </Form>
+            </CommentGroup>
+        </Fragment>
     )
 };
 
